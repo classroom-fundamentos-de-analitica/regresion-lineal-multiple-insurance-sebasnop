ad0c52245fde17963a8bbad5eac6f1c415d83d87
Sebastian Valencia Zapata <sevalenciaz@unal.edu.co>
2023-06-18 13:53:30 -0500

    Probando Pregunta 3

--- preguntas.py
+++ preguntas.py
@@ -67,62 +67,72 @@ def pregunta_03():
 
     # Importe make_column_selector
     # Importe make_column_transformer
+    from sklearn.compose import make_column_selector, make_column_transformer#, ColumnTransformer
+    
     # Importe SelectKBest
     # Importe f_regression
-    # Importe LinearRegression
-    # Importe GridSearchCV
-    # Importe Pipeline
-    # Importe OneHotEncoder
-    from ____ import ____
+    from sklearn.feature_selection import SelectKBest, f_regression
 
-    pipeline = ____(
+    # Importe LinearRegression
+    from sklearn.linear_model import LinearRegression
+
+    # Importe GridSearchCV
+    from sklearn.model_selection import GridSearchCV
+
+    # Importe Pipeline
+    from sklearn.pipeline import Pipeline
+
+    # Importe OneHotEncoder
+    from sklearn.preprocessing import OneHotEncoder
+
+    pipeline = Pipeline(
         steps=[
             # Paso 1: Construya un column_transformer que aplica OneHotEncoder a las
             # variables categóricas, y no aplica ninguna transformación al resto de
             # las variables.
             (
                 "column_transfomer",
-                ____(
+                make_column_transformer(
                     (
-                        ____(),
-                        ____(____=____),
+                        OneHotEncoder(),
+                        make_column_selector(dtype_include=object), # Tambien se puede con ['categorical_column']
                     ),
-                    remainder=____,
+                    remainder='passthrough',
                 ),
             ),
             # Paso 2: Construya un selector de características que seleccione las K
             # características más importantes. Utilice la función f_regression.
             (
                 "selectKBest",
-                ____(____=____),
+                SelectKBest(score_func=f_regression),
             ),
             # Paso 3: Construya un modelo de regresión lineal.
             (
-                "____",
-                ____(____),
+                "linearRegression",
+                LinearRegression(),
             ),
         ],
     )
 
-    # Cargua de las variables.
-    X_train, _, y_train, _ = pregunta_02()
+    # Carga de las variables.
+    X_train, X_test, y_train, y_test = pregunta_02()
 
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar valores desde 1 hasta 11 regresores para el modelo
     param_grid = {
-        ____: ____(____, ____),
+        "selectKBest__k": range(1, 11),
     }
 
     # Defina una instancia de GridSearchCV con el pipeline y el diccionario de
     # parámetros. Use cv = 5, y como métrica de evaluación el valor negativo del
     # error cuadrático medio.
-    gridSearchCV = ____(
-        estimator=____,
-        param_grid=____,
-        cv=____,
-        scoring=____,
-        refit=____,
-        return_train_score=____,
+    gridSearchCV = GridSearchCV(
+        estimator=pipeline,
+        param_grid=param_grid,
+        cv=5,
+        scoring="neg_mean_squared_error",
+        refit=True,
+        return_train_score=True,
     )
 
     # Búsque la mejor combinación de regresores
